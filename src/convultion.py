4cbed35d49c036291bc1990ac636ae978c9a42fa
Fedotov-Dmitriy <qwertyyy20262@mail.ru>
2026-03-26 21:39:06 +0300

    modified main.py for the command line and changed the variable name in convultion.py

--- src/convultion.py
+++ src/convultion.py
@@ -8,14 +8,14 @@ def convultion(image: np.ndarray, kernles: np.ndarray) -> np.ndarray:
     picture = np.zeros((height, width), dtype=np.float32)
     for i in range(height):
         for j in range(width):
-            pixel = 0
-            for sosed_x in range(-kernel_half, kernel_half + 1):
-                for sosed_y in range(-kernel_half, kernel_half + 1):
-                    px = reflect_index(i + sosed_x, image.shape[0])
-                    py = reflect_index(j + sosed_y, image.shape[1])
+            pixel = 0.0
+            for moving_x in range(-kernel_half, kernel_half + 1):
+                for moving_y in range(-kernel_half, kernel_half + 1):
+                    px = reflect_index(i + moving_x, image.shape[0])
+                    py = reflect_index(j + moving_y, image.shape[1])
                     pixel += (
                         image[px][py]
-                        * kernles[kernel_half + sosed_x][kernel_half + sosed_y]
+                        * kernles[kernel_half + moving_x][kernel_half + moving_y]
                     )
             picture[i, j] = pixel
     return np.clip(picture, 0, 255).astype(np.uint8)
--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ from kernels import kernels
 from convultion import convultion
 from PIL import Image
 from numpy import array
+from cli import kernels_output, picture_output
 
 if __name__ == "__main__":
-    img = Image.open("input/b9f91113198e9e6c0a0a0415af19d2b8.jpg").convert("L")
-    result = convultion(array(img), kernels["sharpen"])
+    img = Image.open(picture_output()).convert("L")
+    result = convultion(array(img), kernels[kernels_output()])
     new_img = Image.fromarray(result)
-    new_img.save("output/rabbit_sharpen.jpg")
+    new_img.show()

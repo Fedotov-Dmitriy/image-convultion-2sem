dda0978e5b967b045c0a90e26685400617933b70
Fedotov-Dmitriy <qwertyyy20262@mail.ru>
2026-03-26 21:35:58 +0300

    refactoring for ruff format

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,13 @@
 import os
 
 from kernels import kernels
+
+
 def kernels_output() -> str:
     print("Выберите номер ядра свертки")
-    for i ,name in enumerate(kernels.keys()):
-        print(f"{i+1}.{name}")
-    number = int(input())-1
+    for i, name in enumerate(kernels.keys()):
+        print(f"{i + 1}.{name}")
+    number = int(input()) - 1
     return list(kernels.keys())[number]
 
 
@@ -13,7 +15,7 @@ def picture_output() -> str:
     folder_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "input")
     pictures = os.listdir(folder_path)
     print("Выберите картинку")
-    for i,name in enumerate(pictures):
-        print(f"{i+1}.{name}")
-    number = int(input())-1
-    return os.path.join(folder_path,pictures[number])
+    for i, name in enumerate(pictures):
+        print(f"{i + 1}.{name}")
+    number = int(input()) - 1
+    return os.path.join(folder_path, pictures[number])

f7b27f62f2a47435e53484dcd839a28747b98e3e
Fedotov-Dmitriy <qwertyyy20262@mail.ru>
2026-05-16 15:12:24 +0300

    add validation

--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
-from src.kernels import kernels
-from src.convolution import convolution
-from PIL import Image
+from kernels import kernels
+from convolution import convolution
+from PIL import Image,UnidentifiedImageError
 from numpy import array
-from src.cli import kernels_output, picture_output, select
+from cli import kernels_output, picture_output, select,grayscale
+
+
+def main() -> None:
+    try:
+        img = Image.open(picture_output())
+        img = grayscale(img)
+
+        result = convolution(array(img), kernels[kernels_output()], select())
+
+        new_img = Image.fromarray(result)
+        new_img.show()
+
+    except FileNotFoundError as error:
+        print(f"Ошибка: {error}")
+
+    except UnidentifiedImageError:
+        print("Ошибка: выбранный файл не является изображением")
+
+    except ValueError as error:
+        print(f"Ошибка: {error}")
+
+    except KeyboardInterrupt:
+        print("\nПрограмма остановлена пользователем")
+
 
 if __name__ == "__main__":
-    img = Image.open(picture_output())
-    result = convolution(array(img), kernels[kernels_output()], select())
-    new_img = Image.fromarray(result)
-    new_img.show()
+    main()
